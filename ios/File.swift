d9725344bef875bcd366ac4c3fd23f9c632aeb60
Elena Anghel <elena.anghel@endava.com>
2021-07-29 23:17:49 +0300

    lotie work

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  easy2eat
+//
+//  Created by Elena Anghel on 29.07.2021.
+//
+
+import Foundation
--- src/screens/GetStarted/index.tsx
+++ src/screens/GetStarted/index.tsx
@@ -1,21 +1,34 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View, Text, Pressable, SafeAreaView } from 'react-native';
+import { View, Dimensions,Text, Pressable, SafeAreaView } from 'react-native';
+import LottieView from 'lottie-react-native';
 import { Typography, MainColor } from '../../theme';
 import styles from "./styles";
 
 import InstagramSvg from '../../assets/images/insta.svg';
+
+const screen = Dimensions.get("screen");
+
 
 const GetStarted : React.FC = () => {
 
     const navigation = useNavigation();
 
     return (
-        <SafeAreaView style={styles.container}>
-            <Text style={Typography.title}>Get Started</Text>
-            <InstagramSvg width={20} height={20}/>
-            <Pressable onPress={() => navigation.navigate('Login')}><Text>Go to login</Text></Pressable>
-        </SafeAreaView>
+        <View style={{ width: '100%', height:'100%' }}>
+            {/*<Text style={Typography.title}>Get Started</Text>*/}
+            {/*<InstagramSvg width={20} height={20}/>*/}
+            {/*<Pressable onPress={() => navigation.navigate('Login')}><Text>Go to login</Text></Pressable>*/}
+            <LottieView
+              style={{
+                width: 300,
+                aspectRatio: 300 / 600,
+                flexGrow: 1,
+                alignSelf: 'center',
+              }}
+              resizeMode="cover"
+              source={require('./../../assets/mainanimation.json')} autoPlay loop={false}/>
+        </View>
     )
 }
 
