449af2bff506165a63635741f2ab19791f488ac8
Elena Anghel <elena.anghel@endava.com>
2021-07-29 23:36:56 +0300

    added video bg

--- src/screens/GetStarted/index.tsx
+++ src/screens/GetStarted/index.tsx
@@ -1,9 +1,9 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View, Dimensions,Text, Pressable, SafeAreaView } from 'react-native';
-import LottieView from 'lottie-react-native';
+import { View, Dimensions, Text, Pressable, SafeAreaView, StyleSheet } from "react-native";
+import Video from "react-native-video";
 import { Typography, MainColor } from '../../theme';
-import styles from "./styles";
+// import styles from "./styles";
 
 import InstagramSvg from '../../assets/images/insta.svg';
 
@@ -16,20 +16,32 @@ const GetStarted : React.FC = () => {
 
     return (
         <View style={{ width: '100%', height:'100%' }}>
-            {/*<Text style={Typography.title}>Get Started</Text>*/}
-            {/*<InstagramSvg width={20} height={20}/>*/}
-            {/*<Pressable onPress={() => navigation.navigate('Login')}><Text>Go to login</Text></Pressable>*/}
-            <LottieView
-              style={{
-                width: 300,
-                aspectRatio: 300 / 600,
-                flexGrow: 1,
-                alignSelf: 'center',
-              }}
-              resizeMode="cover"
-              source={require('./../../assets/mainanimation.json')} autoPlay loop={false}/>
+          <Video
+            source={require("../../assets/videomain.mp4")}
+            style={styles.backgroundVideo}
+            muted={true}
+            repeat={false}
+            resizeMode={"cover"}
+            rate={1.0}
+            ignoreSilentSwitch={"obey"}
+          />
+          <View style={{width: '100%', height:'100%', justifyContent: 'center' }}>
+            <Text style={{ fontSize: 50 }}>adfda</Text>
+          </View>
         </View>
     )
 }
 
 export default GetStarted;
+
+const styles = StyleSheet.create({
+  backgroundVideo: {
+    height: '100%',
+    position: "absolute",
+    top: 0,
+    left: 0,
+    alignItems: "stretch",
+    bottom: 0,
+    right: 0
+  }
+});
