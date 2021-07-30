f60603e407794a1d8aa25b247908378c2f10f1df
dorinpuscasu <dorinpuscasu25>
2021-07-30 15:18:17 +0300

    started page

--- src/screens/GetStarted/index.tsx
+++ src/screens/GetStarted/index.tsx
@@ -1,19 +1,17 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View, Dimensions, Text, Pressable, SafeAreaView, StyleSheet } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import Video from "react-native-video";
-import { Typography, MainColor } from '../../theme';
 import styles from "./styles";
 
-import InstagramSvg from '../../assets/images/insta.svg';
-
-const screen = Dimensions.get("screen");
-
+import ArrowRight from '../../assets/images/arrow-right.svg';
+import FacebookIcon from '../../assets/images/socialsicons/facebook.svg';
+import InstagramIcon from '../../assets/images/socialsicons/instagram.svg';
+import TwitterIcon from '../../assets/images/socialsicons/twitter.svg';
 
 const GetStarted : React.FC = () => {
 
     const navigation = useNavigation();
-
     return (
         <View style={{ width: '100%', height:'100%' }}>
           <Video
@@ -26,8 +24,22 @@ const GetStarted : React.FC = () => {
             ignoreSilentSwitch={"obey"}
           />
           <View style={styles.contentWrapper}>
-            <Text style={{ fontSize: 50 }}>adfda</Text>
-            <Pressable onPress={() => navigation.navigate('Login')}><Text>Login</Text></Pressable>
+            <Pressable style={styles.startBtn} onPress={() => navigation.navigate('Login')}>
+              <Text style={styles.startBtnText}>Start</Text>
+              <ArrowRight/>
+            </Pressable>
+            <View style={styles.socialBtns}>
+              <Pressable style={{ marginRight: 11 }}>
+                <FacebookIcon/>
+              </Pressable>
+              <Pressable style={{ marginRight: 11 }}>
+                <InstagramIcon/>
+              </Pressable>
+              <Pressable>
+                <TwitterIcon/>
+              </Pressable>
+            </View>
+            <Text style={styles.copyText}>Easy2Eat Mobile App © 2021 All rights reserved</Text>
           </View>
         </View>
     )
---
+++ src/screens/Search/index.tsx
@@ -0,0 +1,18 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { View, Text, Pressable } from "react-native";
+import Video from "react-native-video";
+import styles from "./styles";
+
+
+const Search : React.FC = () => {
+
+    const navigation = useNavigation();
+    return (
+        <View style={{ width: '100%', height:'100%' }}>
+
+        </View>
+    )
+}
+
+export default Search;
